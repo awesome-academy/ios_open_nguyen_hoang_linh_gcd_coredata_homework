73b476c01719738093192a249e872d2edb362b54
Hoang Linh <hlinh.jobs@gmail.com>
2023-08-25 17:02:23 +0700

    Init project

---
+++ UserList/UserList/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  UserList
+//
+//  Created by Hoang Linh Nguyen on 25/8/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ UserList/UserList/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  UserList
+//
+//  Created by Hoang Linh Nguyen on 25/8/2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
